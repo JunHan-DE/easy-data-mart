cdc4a43b2f51b06137f187ec0d8d6e9df0022f14
Jun <jun@chai.finance>
2022-06-28 01:12:46 +0900

    chore: delete unused annotation

--- rest_api_client_server/main.py
+++ rest_api_client_server/main.py
@@ -1,4 +1,3 @@
-import asyncio
 import typing
 
 from confluent_kafka import KafkaException
@@ -9,28 +8,83 @@ from ksql import KSQLAPI
 
 import uvicorn
 from app.utils.kafka.aio_producer import AIOProducer
-from app.sql.ksql import create_stream_sql
-from app.common.variables import (
+from app.sql.ksql import (
+    create_stream_ksql,
+    create_postgres_source_connect_ksql,
+    create_es_sink_connect_ksql,
+)
+from app.utils.database.models import (
     EtlConfig,
-    Item,
+    Items,
 )
 from app.common.config import conf
+from app.common.variables import db_connector_mapper
 
 app = FastAPI()
 aio_producer: typing.Union[AIOProducer, None] = None
 admin_client: typing.Union[AdminClient, None] = None
 ksql_client: typing.Union[KSQLAPI, None] = None
 conf = conf()
+cnt = 0
 
-# BOOTSTRAP_SERVERS = {
-#     "bootstrap.servers": "jun-kafka-0.jun-kafka-headless.jun-test-kafka.svc.cluster.local:9092,"
-#                          "jun-kafka-1.jun-kafka-headless.jun-test-kafka.svc.cluster.local:9092,"
-#                          "jun-kafka-2.jun-kafka-headless.jun-test-kafka.svc.cluster.local:9092"
-# }
 BOOTSTRAP_SERVERS = {
     "bootstrap.servers": "0.0.0.0:9092"
 }
 KSQL_SERVER = "http://0.0.0.0:8088"
+
+
+@app.post("/etl")
+async def run_etl_code(config: EtlConfig):
+
+    topic = (
+        config.source_table_name
+        if config.source_table_name == config.target_table_name
+        else f"test_{config.source_table_name}_to_{config.target_table_name}"
+    )
+
+    source_connect = db_connector_mapper.get(config.source_type)
+    target_connect = db_connector_mapper.get(config.target_type)
+
+    # 1. create topic
+    topics = [NewTopic(topic=topic, num_partitions=1, replication_factor=1)]
+    admin_client.create_topics(topics)
+
+    # 2. add streams
+    ksql_client.ksql(
+        create_stream_ksql.format(
+            topic=topic,
+            column_info=config.source_table_columns,
+        )
+    )
+
+    # 2. add source kafka connect
+    # TODO: Add conditions to Support more source connectors
+    ksql_client.ksql(
+        create_postgres_source_connect_ksql.format(
+            db_name=config.source_db_name,
+            db_password=config.source_db_password,
+            db_port=config.source_db_port,
+            db_user=config.source_db_user,
+            extract_key_field=config.extract_key_field,
+        )
+    )
+
+    # 3. add sink kafka connect
+    ksql_client.ksql(
+        create_es_sink_connect_ksql.format(
+            topic=topic,
+        )
+    )
+
+    return {
+        "is_success": True,
+        "message": "ETL has been finished and Data Ingestion from source to target is working in progress."
+    }
+
+
+def ack(err, msg):
+    global cnt
+    cnt = cnt + 1
 
 
 @app.on_event("startup")
@@ -52,90 +106,9 @@ async def check_server_health_status() -> dict:
     return {"status": 200}
 
 
-@app.post("/etl")
-async def run_etl_code(config: EtlConfig):
 
-    topic = (
-        config.source_table_name
-        if config.source_table_name == config.target_table_name
-        else f"jdbc_{config.source_table_name}_to_{config.target_table_name}"
-    )
-
-    # 1. create topic
-    topics = [NewTopic(topic=topic, num_partitions=3, replication_factor=2)]
-    admin_client.create_topics(topics)
-
-    # 2. add streams
-    # ksql_client.create_stream(
-    #     table_name=topic,
-    #     columns_type=["is_success VARCHAR"],
-    #     topic=topic,
-    # )
-
-    ksql_client.ksql("""CREATE STREAM JDBC_CUSTOMERS_TO_CUSTOMER (IS_SUCCESS STRING) WITH (KAFKA_TOPIC='jdbc_customers_to_customer', PARTITIONS=2, REPLICAS=1, VALUE_FORMAT='JSON')""")
-    # 2. add source kafka connect
-    # ksql_client.ksql(f"""
-    #     CREATE SOURCE CONNECTOR IF NOT EXISTS `{topic}-jdbc-connector` WITH(
-    #     "connector.class"='io.confluent.connect.jdbc.JdbcSourceConnector',
-    #     "connection.url"='jdbc:postgresql://{config.source_db_host}:{config.source_db_port}/{config.source_db_name}',
-    #     "mode"='bulk',
-    #     "topic.prefix"='jdbc-',
-    #     "table.whitelist"='users',
-    #     "key"='username');
-    # """)
-    ksql_client.ksql(f"""CREATE SOURCE CONNECTOR `demo-source-connector` WITH ('connector.class'               = 'io.debezium.connector.postgresql.PostgresConnector',
-  'database.dbname'               = 'demo',
-  'database.hostname'             = 'postgres',
-  'database.password'             = 'postgres',
-  'database.port'                 = '5432',
-  'database.server.name'          = 'workshop_pg',
-  'database.user'                 = 'postgres',
-  'plugin.name'                   = 'pgoutput',
-  'snapshot.mode'                 = 'always',
-  'table.whitelist'               = 'public.customers,public.companies',
-  'transforms'                    = 'extractKey,extractValue',
-  'transforms.extractKey.field'   = 'id',
-  'transforms.extractKey.type'    = 'org.apache.kafka.connect.transforms.ExtractField$Key',
-  'transforms.extractValue.field' = 'after',
-  'transforms.extractValue.type'  = 'org.apache.kafka.connect.transforms.ExtractField$Value');""")
-
-    # 3. add sink kafka connect
-    # ksql_client.ksql(f"""
-    #     CREATE SINK CONNECTOR IF NOT EXISTS `{topic}-jdbc-connector` WITH(
-    #     "connector.class"='io.confluent.connect.jdbc.JdbcSourceConnector',
-    #     "connection.url"='jdbc:postgresql://{config.source_db_host}:{config.source_db_port}/{config.source_db_name}',
-    #     "mode"='bulk',
-    #     "topic.prefix"='jdbc-',
-    #     "table.whitelist"='users',
-    #     "key"='username');
-    # """)
-    ksql_client.ksql(f"""CREATE SINK CONNECTOR `workshop-es-sink-connector` WITH (
-  'behavior.on.null.values' = 'delete',
-  'connector.class'         = 'io.confluent.connect.elasticsearch.ElasticsearchSinkConnector',
-  'connection.url'          = 'http://elasticsearch:9200',
-  'schema.ignore'           = 'true',
-  'tasks.max'               = '1',
-  'topics'                  = 'jdbc_customers_to_customer',
-  'type.name'               = 'ksql-workshop',
-  'write.method'            = 'upsert'
-);""")
-    print("ksql started to add source and sink JDBC connector ")
-
-    return {
-        "is_success": True
-    }
-
-
-cnt = 0
-
-
-def ack(err, msg):
-    global cnt
-    cnt = cnt + 1
-
-
-@app.post("/items2")
-async def create_item2(item: Item):
+@app.post("/produce/items")
+async def create_item2(item: Items):
     try:
         aio_producer.produce("items", item.name, on_delivery=ack)
         return {"timestamp": time()}
